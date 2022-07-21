7f845e19aaa78dfc7a92a17f487a73d1c90b9348
ivanprofpv <akvabanka@gmail.com>
2022-07-21 20:01:44 +0300

    fix: revision files lesson 8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    "https://github.com/#{author}/#{repo}"
+    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: "_blank"
   end
-  
+
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,18 +1,3 @@
 module TestsHelper
 
-  # def test_level(test)
-  #   case test.level
-  #   when 0
-  #     easy
-  #   when 1
-  #     elementary
-  #   when 2
-  #     advanced
-  #   when 3
-  #     hard
-  #   else
-  #     hero
-  #   end
-  # end
-
 end
