678d84f076df842e416cd5f74ddbc168552aea15
ivanprofpv <akvabanka@gmail.com>
2022-07-28 21:29:13 +0300

    fix: test_passage.rb, рефакторинг колбэка

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_next_question
+  before_validation :before_validation_set_question, on: :create
 
   SUCCESS_RATE = 85
 
@@ -13,10 +12,8 @@ class TestPassage < ApplicationRecord
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-  
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+
     save!
   end
 
@@ -29,14 +26,14 @@ class TestPassage < ApplicationRecord
   end
 
   def question_nubmer
-    self.test.questions.where('id <= ?', self.current_question.id).count
+    self.test.questions.where('id <= ?', current_question.id).count
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
+  # def before_validation_set_first_question
+  #   self.current_question = test.questions.first if test.present?
+  # end
 
   def correct_answer?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
@@ -46,7 +43,11 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
-  def before_update_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_validation_set_question
+    self.current_question = if test.present?
+                              test.questions.first
+                            else
+                              test.questions.order(:id).where('id > ?', current_question.id).first
+                            end
   end
 end
