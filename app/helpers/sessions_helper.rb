f15ed49ea6321e93bdf7e9e34cccfa4fc2f6bf36
ivanprofpv <akvabanka@gmail.com>
2022-08-03 16:32:48 +0400

    fix: correction of remarks for lesson10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,9 @@ module SessionsHelper
 
   def flash_message
     flash.map do |key_class, msg|
-      content_tag :div, msg, class: "flash #{key_class}"
+      if msg.present?
+        content_tag :div, msg, class: "flash #{key_class}"
+      end
     end.join
   end
 
