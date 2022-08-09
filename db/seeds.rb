94e3a2e1d3535b7759c07ed72b0efc43c22ed9f7
ivanprofpv <akvabanka@gmail.com>
2022-08-09 11:50:06 +0400

    fix: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
+Admin.create(
+  email: 'admin@testguru.com',
+  password: '12345',
+  password_confirmation: '12345',
+  first_name: "FirstName",
+  last_name: "LastName",
+  confirmed_at: Time.zone.now
+)
+
 3.times do |i|
   User.create(
     email: "email-#{i}@mail.com",
-    password: "1234_#{i}"
+    password: '1234',
+    password_confirmation: '1234'
+    first_name: "First_name_#{i}",
+    last_name: "Last_name_#{i}",
+    confirmed_at: Time.zone.now
   )
 end
 
@@ -9,19 +22,19 @@ backend = Category.create(title: 'Backend')
 fronted = Category.create(title: 'Fronted')
 mobile = Category.create(title: 'mobile')
 
-ruby_test = Test.create(title: 'Ruby', level: 3)
-go_test = Test.create(title: 'Go', level: 2)
-html_test = Test.create(title: 'HTML', level: 1)
-css_test = Test.create(title: 'CSS', level: 1)
-js_test = Test.create(title: 'JS', level: 2)
-swift_test = Test.create(title: 'Swift', level: 3)
+ruby_test = backend.tests.create(title: 'Ruby', level: 3)
+go_test = backend.tests.create(title: 'Go', level: 2)
+html_test = fronted.tests.create(title: 'HTML', level: 1)
+css_test = fronted.tests.create(title: 'CSS', level: 1)
+js_test = mobile.tests.create(title: 'JS', level: 2)
+swift_test = mobile.tests.create(title: 'Swift', level: 3)
 
-ruby_question = Question.create(title: 'Ruby its Backend language?')
-go_question = Question.create(title: 'Go its Backend language?')
-html_question = Question.create(title: 'HTML its Fronted language?')
-css_question = Question.create(title: 'CSS its Fronted language?')
-js_question = Question.create(title: 'JS its Mobile language?')
-swift_question = Question.create(title: 'Swift its Mobile language?')
+ruby_question = ruby_test.questions.create(title: 'Ruby its Backend language?')
+go_question = go_test.questions.create(title: 'Go its Backend language?')
+html_question = html_test.questions.create(title: 'HTML its Fronted language?')
+css_question = css_test.questions.create(title: 'CSS its Fronted language?')
+js_question = js_test.questions.create(title: 'JS its Mobile language?')
+swift_question = swift_test.questions.create(title: 'Swift its Mobile language?')
 
 ruby_question.answers.create([
                 {title: 'Yes', question_id: questions[0].id, correct: true},
