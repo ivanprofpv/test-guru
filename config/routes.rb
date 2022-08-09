41efe14fbf6648a1cdff8fd5bbd7887ff1cd5a1f
ivanprofpv <akvabanka@gmail.com>
2022-08-09 09:05:47 +0400

    fix: routes.rb edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root 'tests#index'
 
-  post   :login,  to: 'sessions#create'
-  delete :logout, to: 'sessions#delete'
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
 
     member do
       post :start
@@ -23,7 +18,11 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
+    end
   end
 
 end
