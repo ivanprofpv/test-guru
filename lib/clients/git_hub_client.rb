104f1e6620efe405122a5660b6a98290d1e32bc7
ivanprofpv <akvabanka@gmail.com>
2022-08-18 08:09:32 +0400

    fix: скрытый токен в git_hub_client и правка в файле show - Atom туда вставил какие-то теги

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,7 +1,7 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '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'
+  ACCESS_TOKEN = ENV.fetch('GITHUB_ACCESS_TOKEN')
 
   def initialize
     @http_client = setup_http_client
