f870a9048faf33efee4a57e189ff29656ec60db2
ivanprofpv <akvabanka@gmail.com>
2022-09-08 16:05:49 +0400

    files for lesson16

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,9 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_tests, only: %i[index update]
+  before_action :set_test, only: %i[show edit update destroy start update_inline]
 
   def index
-    @tests = Test.all
   end
 
   def new
@@ -36,6 +36,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
 
     @test.destroy
@@ -44,6 +52,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def set_test
     @test = Test.find(params[:id])
