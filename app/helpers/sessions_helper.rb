e1da1796be5934f4a8419253b1fa93841c6fe39f
ivanprofpv <akvabanka@gmail.com>
2022-09-13 14:07:47 +0400

    files for lesson 16 and correction of past mistakes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,3 @@
 module SessionsHelper
 
-  def flash_message
-    flash.map do |key_class, msg|
-      if msg.present?
-        content_tag :div, msg, class: "flash #{key_class}"
-      end
-    end.join
-  end
-
 end
