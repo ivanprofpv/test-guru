1be55a4ce8af560bb51a5d389e12dc802e3722f8
ivanprofpv <akvabanka@gmail.com>
2022-10-06 14:11:07 +0400

    восстановленный файл

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -26,7 +26,11 @@ class BadgeService
   end
 
   def first_rule_completed?(badge)
-    @current_test.test_passages.where(user: @current_user, success: true).count == 1
+    completed = @current_user.test_passages
+                  .where(test: @current_test, passed: true)
+                  .pluck(:test_id).uniq
+    first_test = TestPassage.where(user: @current_user, test: @current_test)
+    first_test.count == 1 && completed.count == 1
   end
 
   def level_rule_completed?(rule_value)
